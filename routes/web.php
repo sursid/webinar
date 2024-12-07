00bf7072fd11c3ae5051eeee8b53d5d733461522
SURS ID <sursid@ymgamestore.com>
2024-12-07 20:47:47 +0700

    Kedua

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,18 @@
 <?php
 
-use App\Http\Controllers\WebinarController;
-use App\Http\Controllers\CertificateController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Webinar Registration & Certificate Routes
+| Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
 
-Route::group(['prefix' => 'webinar'], function () {
-    // Registration Routes
-    Route::get('/register', [WebinarController::class, 'showRegistrationForm'])->name('webinar.register');
-    Route::post('/register', [WebinarController::class, 'processRegistration'])->name('webinar.register.store');
-    Route::get('/registration/success', [WebinarController::class, 'registrationSuccess'])->name('webinar.register.success');
-    
-    // Certificate Routes
-    Route::group(['prefix' => 'certificate'], function () {
-        Route::get('/verify/{token}', [CertificateController::class, 'verify'])->name('certificate.verify');
-        Route::get('/download/{token}', [CertificateController::class, 'download'])->name('certificate.download');
-        Route::get('/generate/{participantId}', [CertificateController::class, 'generate'])
-            ->name('certificate.generate')
-            ->middleware('auth'); // Protect certificate generation
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
-
-// Redirect root to registration page
-Route::redirect('/', '/webinar/register');
